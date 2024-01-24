3d81cd65401bc4db4063f0cf236cafd2dfe63544
WUStudent <wustudent@webster.edu>
2024-01-24 16:47:18 -0600

    ship and projectile mechanics

--- Space Shooter Unity/Assets/Code/Ship.cs
+++ Space Shooter Unity/Assets/Code/Ship.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Ship : MonoBehaviour
 {
     public Rigidbody2D rigidBody2D;
-    public GameObject projectile;
+    public GameObject projectilePrefab;
     public Transform projectileSpawnPoint;
 
     public float acceleration;
@@ -17,23 +17,39 @@ public class Ship : MonoBehaviour
     public float currentSpeed;
     public int currentHealth;
 
+    private void FixedUpdate()
+    {
+        if (rigidBody2D.velocity.magnitude > maxSpeed)
+        { 
+            rigidBody2D.velocity = rigidBody2D.velocity.normalized * maxSpeed;
+        }
+    }
+
     public void Thrust()
     {
-        rigidBody2D.AddForce(Vector2.up * acceleration);
+        rigidBody2D.AddForce(transform.up * acceleration);
     }
 
     public void Shoot()
-    { 
-    
+    {
+        GameObject projectile = Instantiate(projectilePrefab, projectileSpawnPoint.position, transform.rotation);
+        projectile.GetComponent<Rigidbody2D>().AddForce(transform.up * projectileSpeed);
+        projectile.GetComponent<Projectile>().GetFired(gameObject);
+        Destroy(projectile, 4);
     }
 
-    public void TakeDamage()
-    { 
-    
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        if (currentHealth <= 0)
+        {
+            Explode();
+        }   
     }
 
     public void Explode()
     { 
-    
+        Destroy(gameObject);
     }
 }
