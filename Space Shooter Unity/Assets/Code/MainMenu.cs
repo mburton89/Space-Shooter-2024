e71fecc693e99e2a13fc27c23c1736cdd30c2ba0
WUStudent <wustudent@webster.edu>
2024-03-20 15:09:28 -0500

    Added comment to main menu script

--- Space Shooter Unity/Assets/Code/MainMenu.cs
+++ Space Shooter Unity/Assets/Code/MainMenu.cs
@@ -10,6 +10,8 @@ public class MainMenu : MonoBehaviour
     public Button startButton;
     public TextMeshProUGUI highestWaveText;
 
+    //supr serious msg
+
     void Start()
     {
         startButton.onClick.AddListener(HandleStartButtonClicked);
